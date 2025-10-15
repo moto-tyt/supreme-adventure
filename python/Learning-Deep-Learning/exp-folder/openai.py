f39f4c0097f0243b426e640c98e7419411a6fa79
moto-tyt <motoakitoyota@hotmail.com>
2025-10-15 18:23:16 -0400

    small commit

---
+++ python/Learning-Deep-Learning/exp-folder/openai.py
@@ -0,0 +1,9 @@
+from openai import OpenAI
+client = OpenAI()
+
+response = client.responses.create(
+    model="gpt-5",
+    input="Write a short bedtime story about a unicorn."
+)
+
+print(response.output_text)
